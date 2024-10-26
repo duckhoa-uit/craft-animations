7d4171b1795e7dd34b42ae9c884b2bdb8516b95c
DeeNKay <duckhoa.dev@gmail.com>
2024-10-26 11:23:38 +0700

    fix: build scroll-driven-image-sequence error

--- app/scroll-driven-image-sequence/page.tsx
+++ app/scroll-driven-image-sequence/page.tsx
@@ -1,7 +1,11 @@
-import { ScrollSequence } from "@/components/image-sequence";
+import dynamic from "next/dynamic";
+
+const ScrollSequence = dynamic(
+  () =>
+    import("../../components/image-sequence").then((mod) => mod.ScrollSequence),
+  { ssr: false },
+);
 
 export default function ScrollDriven() {
-  return (
-        <ScrollSequence />
-  );
+  return <ScrollSequence />;
 }
